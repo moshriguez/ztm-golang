2be911df3675288182f6e4961a1d05bc6c174437
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2022-12-27 00:04:46 -0800

    struct exercise complete

--- src/lectures/exercise/structs/structs.go
+++ src/lectures/exercise/structs/structs.go
@@ -17,8 +17,55 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Coordinate struct {
+	x, y int
+}
+
+type Rectangle struct {
+	a Coordinate // top left
+	b Coordinate // bottom right
+}
+
+func width(rectangle *Rectangle) int {
+	return (rectangle.b.x - rectangle.a.x)
+}
+
+func length(rectangle *Rectangle) int {
+	return (rectangle.a.y - rectangle.b.y)
+}
+
+
+func Area(rectangle *Rectangle) int {
+	area := length(rectangle) * width(rectangle)
+	fmt.Println("Area of rectangle,", rectangle, ":", area)
+	return area
+}
+
+func Perimeter(rectangle *Rectangle) int {
+	perimeter := (length(rectangle) + width(rectangle)) * 2
+	fmt.Println("Perimeter of rectangle,", rectangle, ":", perimeter)
+	return perimeter
+}
+
+func Double(rectangle *Rectangle) {
+	length := length(rectangle)
+	width := width(rectangle)
+	rectangle.b.x += length
+	rectangle.b.y -= width
+}
 
 func main() {
+	coorA := Coordinate{1, 5}
+	coorB := Coordinate{5, 1}
+	square := Rectangle{coorA, coorB}
+	Area(&square)
+	Perimeter(&square)
+	Double(&square)
+	Area(&square)
+	Perimeter(&square)
 
 }
