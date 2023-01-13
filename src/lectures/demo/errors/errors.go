96e4178d552ee3785e8fc68aa10b42a203435dda
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2023-01-12 19:34:20 -0800

    error demo complete

--- src/lectures/demo/errors/errors.go
+++ src/lectures/demo/errors/errors.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
-func main() {
+type Stuff struct {
+	values []int
+}
 
+func (s *Stuff) Get(i int) (int, error) {
+	if i > len(s.values) {
+		return 0, errors.New(fmt.Sprintf("no element at index %v", i))
+	} else {
+		return s.values[i], nil
+	}
+}
+
+func main() {
+	stuff := Stuff{}
+	value, err := stuff.Get(1)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("value is", value)
+	}
 }
