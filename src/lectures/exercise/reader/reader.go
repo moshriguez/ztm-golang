51a6669ccc73a459fc0c61642dfa285fe804eb52
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2023-01-15 19:15:08 -0800

    reader exercise complete

--- src/lectures/exercise/reader/reader.go
+++ src/lectures/exercise/reader/reader.go
@@ -22,15 +22,45 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
+
 func main() {
-	r := bufio.NewReader(os.Stdin)
+	r := bufio.NewScanner(os.Stdin)
 
 	commandsNum := 0
+	linesNum := 0
 	blankLineNum := 0
 
-	for {
-		input, inputErr := r.
+	for r.Scan() {
+		input := r.Text()
+		if input == "" {
+			blankLineNum += 1
+			fmt.Println("I can say nothing too")
+			fmt.Println()
+		} else if strings.ToLower(input) == "q" {
+			break
+		} else {
+			switch input {
+			case "Hi":
+				fmt.Println("Hi there")
+				commandsNum += 1
+			case "Bye":
+				fmt.Println("See you next time")
+				commandsNum += 1
+			case "What's the secret to life?":
+				fmt.Println("42")
+			default:
+				fmt.Println("I don't know what to say")
+			}
+			linesNum += 1
+		}
 	}
+	fmt.Println("... shutting down ...")
+	fmt.Println()
+	fmt.Println("Program stats:")
+	fmt.Printf("Commands ran: %v | Lines entered: %v | Blank Lines: %v\n", commandsNum, linesNum, blankLineNum)
+	fmt.Println()
+
 }
