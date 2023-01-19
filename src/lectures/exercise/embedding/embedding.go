f52b7e018ed575f3250db98d20da35306e3d11e0
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2023-01-18 23:24:53 -0800

    embedding exercise complete

--- src/lectures/exercise/embedding/embedding.go
+++ src/lectures/exercise/embedding/embedding.go
@@ -23,16 +23,55 @@ type BandwidthUsage struct {
 	amount []Bytes
 }
 
+func (b *BandwidthUsage) average() Bytes {
+	var sum Bytes = 0
+	for _, sample := range b.amount {
+		sum += sample
+	}
+	return sum / Bytes(len(b.amount))
+}
+
 type CpuTemp struct {
 	temp []Celcius
+}
+
+func (c *CpuTemp) average() Celcius {
+	var sum Celcius = 0
+	for _, sample := range c.temp {
+		sum += sample
+	}
+	return sum / Celcius(len(c.temp))
 }
 
 type MemoryUsage struct {
 	amount []Bytes
 }
 
+func (m *MemoryUsage) average() Bytes {
+	var sum Bytes = 0
+	for _, sample := range m.amount {
+		sum += sample
+	}
+	return sum / Bytes(len(m.amount))
+}
+
+type Dashboard struct {
+	BandwidthUsage
+	CpuTemp
+	MemoryUsage
+}
+
+func (d *Dashboard) Print() {
+	fmt.Println("Average Bandwidth Usage:", d.BandwidthUsage.average())
+	fmt.Println("Average CPU Temp:", d.CpuTemp.average())
+	fmt.Println("Average Memory Usage:", d.MemoryUsage.average())
+}
+
 func main() {
 	bandwidth := BandwidthUsage{[]Bytes{50000, 100000, 130000, 80000, 90000}}
 	temp := CpuTemp{[]Celcius{50, 51, 53, 51, 52}}
 	memory := MemoryUsage{[]Bytes{800000, 800000, 810000, 820000, 800000}}
+
+	dash := Dashboard{bandwidth, temp, memory}
+	dash.Print()
 }
