52f8ed392527815391f3523be09fc816edcaebe5
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2023-01-22 18:15:32 -0800

    my go at function literals exercise

--- src/lectures/exercise/func-literal/func-literal.go
+++ src/lectures/exercise/func-literal/func-literal.go
@@ -18,7 +18,63 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode"
+)
+
+func ProcessLineOfText(lines []string, processFn func(input []string)) {
+	processFn(lines)
+}
+
+func PrintNumOfLetters(input []string) {
+	numOfLetters := 0
+	for _, line := range input {
+		for _, rune := range line {
+			if unicode.IsLetter(rune) {
+				numOfLetters += 1
+			}
+		}
+	}
+	fmt.Printf("The text has %v letters\n", numOfLetters)
+}
+
+func PrintNumOfDigits(input []string) {
+	numOfDigits := 0
+	for _, line := range input {
+		for _, rune := range line {
+			if unicode.IsDigit(rune) {
+				numOfDigits += 1
+			}
+		}
+	}
+	fmt.Printf("The text has %v digits\n", numOfDigits)
+}
+
+func PrintNumOfSpaces(input []string) {
+	numOfSpaces := 0
+	for _, line := range input {
+		for _, rune := range line {
+			if unicode.IsSpace(rune) {
+				numOfSpaces += 1
+			}
+		}
+	}
+	fmt.Printf("The text has %v spaces\n", numOfSpaces)
+}
+
+func PrintNumOfPuncuation(input []string) {
+	numOfPunct := 0
+	for _, line := range input {
+		for _, rune := range line {
+			if unicode.IsPunct(rune) {
+				numOfPunct += 1
+			}
+		}
+	}
+	fmt.Printf("The text has %v puncuation marks\n", numOfPunct)
+
+}
 
 func main() {
 	lines := []string{
@@ -28,4 +84,8 @@ func main() {
 		"12 spaces,",
 		"and 4 punctuation marks in these lines of text!",
 	}
+	ProcessLineOfText(lines, PrintNumOfLetters)
+	ProcessLineOfText(lines, PrintNumOfDigits)
+	ProcessLineOfText(lines, PrintNumOfSpaces)
+	ProcessLineOfText(lines, PrintNumOfPuncuation)
 }
