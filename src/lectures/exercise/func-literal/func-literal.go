9b97965285cf3decd54c330ada6304f04fd2f96c
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2023-01-22 18:52:40 -0800

    refactor; less repitition, but a bit brittle; only commiting because there were some good learnings

--- src/lectures/exercise/func-literal/func-literal.go
+++ src/lectures/exercise/func-literal/func-literal.go
@@ -23,58 +23,92 @@ import (
 	"unicode"
 )
 
-func ProcessLineOfText(lines []string, processFn func(input []string)) {
-	processFn(lines)
+type UnicodeFn func(r rune) bool
+
+var runeTypeToFn = map[string]UnicodeFn{
+	"letter": unicode.IsLetter,
+	"digit": unicode.IsDigit,
+	"space": unicode.IsSpace,
+	"punctuation mark": unicode.IsPunct, 
 }
 
-func PrintNumOfLetters(input []string) {
-	numOfLetters := 0
+func ProcessLineOfText(lines []string, runeType string, processFn func(input []string, runeType string)) {
+	processFn(lines, runeType)
+}
+
+func PrintNumOf(input []string, runeType string) {
+	count := 0
+	fn, ok := runeTypeToFn[runeType]
+	if !ok {
+		panic("We don't know what function to run for that runeType")
+	}
 	for _, line := range input {
 		for _, rune := range line {
-			if unicode.IsLetter(rune) {
-				numOfLetters += 1
+			if fn(rune) {
+				count += 1
 			}
 		}
 	}
-	fmt.Printf("The text has %v letters\n", numOfLetters)
+
+	fmt.Printf("The text has %v %v%v\n", count, runeType, plural(count))
 }
 
-func PrintNumOfDigits(input []string) {
-	numOfDigits := 0
-	for _, line := range input {
-		for _, rune := range line {
-			if unicode.IsDigit(rune) {
-				numOfDigits += 1
-			}
-		}
+func plural(count int) string {
+	if count == 1 {
+		return ""
+	} else {
+		return "s"
 	}
-	fmt.Printf("The text has %v digits\n", numOfDigits)
 }
 
-func PrintNumOfSpaces(input []string) {
-	numOfSpaces := 0
-	for _, line := range input {
-		for _, rune := range line {
-			if unicode.IsSpace(rune) {
-				numOfSpaces += 1
-			}
-		}
-	}
-	fmt.Printf("The text has %v spaces\n", numOfSpaces)
-}
+// func PrintNumOfLetters(input []string) {
+// 	numOfLetters := 0
+// 	for _, line := range input {
+// 		for _, rune := range line {
+// 			if unicode.IsLetter(rune) {
+// 				numOfLetters += 1
+// 			}
+// 		}
+// 	}
+// 	fmt.Printf("The text has %v letters\n", numOfLetters)
+// }
 
-func PrintNumOfPuncuation(input []string) {
-	numOfPunct := 0
-	for _, line := range input {
-		for _, rune := range line {
-			if unicode.IsPunct(rune) {
-				numOfPunct += 1
-			}
-		}
-	}
-	fmt.Printf("The text has %v puncuation marks\n", numOfPunct)
+// func PrintNumOfDigits(input []string) {
+// 	numOfDigits := 0
+// 	for _, line := range input {
+// 		for _, rune := range line {
+// 			if unicode.IsDigit(rune) {
+// 				numOfDigits += 1
+// 			}
+// 		}
+// 	}
+// 	fmt.Printf("The text has %v digits\n", numOfDigits)
+// }
 
-}
+// func PrintNumOfSpaces(input []string) {
+// 	numOfSpaces := 0
+// 	for _, line := range input {
+// 		for _, rune := range line {
+// 			if unicode.IsSpace(rune) {
+// 				numOfSpaces += 1
+// 			}
+// 		}
+// 	}
+// 	fmt.Printf("The text has %v spaces\n", numOfSpaces)
+// }
+
+// func PrintNumOfPuncuation(input []string) {
+// 	numOfPunct := 0
+// 	for _, line := range input {
+// 		for _, rune := range line {
+// 			if unicode.IsPunct(rune) {
+// 				numOfPunct += 1
+// 			}
+// 		}
+// 	}
+// 	fmt.Printf("The text has %v puncuation marks\n", numOfPunct)
+
+// }
 
 func main() {
 	lines := []string{
@@ -84,8 +118,8 @@ func main() {
 		"12 spaces,",
 		"and 4 punctuation marks in these lines of text!",
 	}
-	ProcessLineOfText(lines, PrintNumOfLetters)
-	ProcessLineOfText(lines, PrintNumOfDigits)
-	ProcessLineOfText(lines, PrintNumOfSpaces)
-	ProcessLineOfText(lines, PrintNumOfPuncuation)
+	ProcessLineOfText(lines, "letter", PrintNumOf)
+	ProcessLineOfText(lines, "digit", PrintNumOf)
+	ProcessLineOfText(lines, "space", PrintNumOf)
+	ProcessLineOfText(lines, "punctuation mark", PrintNumOf)
 }
